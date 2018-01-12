ecef5422f677687f640726720b85189aadd0a07e
alexsanjoseph <alexsanjoseph@gmail.com>
2018-01-12 13:18:04 +0530

    tes2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>EKAIAS</title>
+        <title>Laravel</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -77,6 +77,11 @@
                 </div>
             @endif
 
+            <div class="content">
+                <div class="title m-b-md">
+                    Laravel
+                </div>
+            </div>
         </div>
     </body>
 </html>
